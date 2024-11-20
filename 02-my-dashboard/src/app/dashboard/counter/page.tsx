b02bdcbdc486a9d033c3b49ec1981689c1a4649a
Fernando Ortiz <fernandodev.ortiz@gmail.com>
2024-11-19 20:44:56 -0600

    Complete section 3

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,7 +1,17 @@
+import type { Metadata } from "next";
+import { CartCounter } from '../../shoppingCart/components/CartCounter';
+
+export const metadata: Metadata = {
+   title: 'Shopping Cart',
+   description: 'Un simple contador',
+}
+
 export default function CounterPage() {
+
    return (
-      <div>
-         <h1>Counter Page</h1>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+         <span>Productos en el carrito</span>
+         <CartCounter value={20} />
       </div>
    );
 }
--- 02-my-dashboard/src/app/page.tsx
+++ 02-my-dashboard/src/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function HomePage() {
 
-  redirect('/dashboard/counter'); // <- esto retorna never
+  redirect('/dashboard/main'); // <- esto retorna never
 }
